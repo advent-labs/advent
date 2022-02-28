96cc6d4a784869575c1d897c9d355ce7be1ce707
Thomas Ruble <sketchbang@gmail.com>
2022-02-28 12:14:47 -0600

    rm modal

--- webclient/src/pages/dash/Dash.tsx
+++ webclient/src/pages/dash/Dash.tsx
@@ -50,11 +50,6 @@ export function Dash() {
       <Modal open={settlementPeriodVisible} onClose={hideSettlementPeriods}>
         <SettlementPeriods />
       </Modal>
-      <div className="modal">
-        <div className="modal-background"></div>
-        <div className="modal-content"></div>
-        <button className="modal-close is-large" aria-label="close"></button>
-      </div>
     </div>
   )
 }
