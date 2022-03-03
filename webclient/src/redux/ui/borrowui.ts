fc8c61e4c3cbe6207a73ae021f3931732cb02a51
jmsdevx <jmsdevx@gmail.com>
2022-03-03 14:37:13 -0600

    create borrow forms

--- webclient/src/redux/ui/borrowui.ts
+++ webclient/src/redux/ui/borrowui.ts
@@ -1,6 +1,6 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
-import { actions as reservesActions } from "../reducer/reserves"
-import { RootState } from ".."
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { actions as reservesActions } from '../reducer/reserves'
+import { RootState } from '..'
 
 export interface FixedBorrowPayload {
   amount: number
@@ -12,17 +12,23 @@ export type BorrowUI = {
   amount: string
   token: string
   busy: boolean
+  tab: string
+  inputVal: string
+  inputTime: string
 }
 
 const initialState: BorrowUI = {
-  duration: "1",
-  amount: "",
-  token: "",
+  duration: '1',
+  amount: '',
+  token: '',
   busy: false,
+  tab: 'Borrow',
+  inputVal: '',
+  inputTime: '',
 }
 
 export const borrowUI = createSlice({
-  name: "borrowui",
+  name: 'borrowui',
   initialState,
   reducers: {
     setAmount: (s: BorrowUI, action: PayloadAction<string>) => {
@@ -39,8 +45,17 @@ export const borrowUI = createSlice({
     },
     borrowSucceed: (s) => {
       s.busy = false
-      s.amount = ""
-      s.duration = "1"
+      s.amount = ''
+      s.duration = '1'
+    },
+    setTab: (s: BorrowUI, action: PayloadAction<string>) => {
+      s.tab = action.payload
+    },
+    inputHasChanged: (s: BorrowUI, action: PayloadAction<string>) => {
+      s.inputVal = action.payload
+    },
+    inputTimeHasChanged: (s: BorrowUI, action: PayloadAction<string>) => {
+      s.inputTime = action.payload
     },
   },
   extraReducers: (builder) => {
@@ -56,4 +71,7 @@ export const actions = borrowUI.actions
 export const selectBorrowUIValues = (s: RootState) => ({
   amount: parseFloat(s.borrowui.amount) || 0,
   duration: parseFloat(s.borrowui.duration) || 0,
+  tab: s.borrowui.tab,
+  inputVal: s.borrowui.inputVal,
+  inputTime: s.borrowui.inputTime,
 })
