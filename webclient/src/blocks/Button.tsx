8f17f00d01b3700caa84f3fc1b8835c474f875b9
jmsdevx <jmsdevx@gmail.com>
2022-03-02 19:26:08 -0600

    add dropdown to button types

--- webclient/src/blocks/Button.tsx
+++ webclient/src/blocks/Button.tsx
@@ -1,18 +1,20 @@
 export interface ButtonProps {
-  type: 'primary' | 'secondary' | 'action' | 'transparent'
+  type: 'primary' | 'secondary' | 'action' | 'transparent' | 'dropdown'
   text: string
   handler: () => void
-  xtraClass?: string
+  xtra?: string
   disabled?: boolean
+  icon?: string
 }
-function Button({ type, handler, text, xtraClass, disabled }: ButtonProps) {
+function Button({ type, handler, text, xtra, disabled, icon }: ButtonProps) {
   return (
     <button
-      className={`button is-${type} ${xtraClass}`}
+      className={`button is-${type} ${xtra}`}
       onClick={handler}
       disabled={disabled}
     >
       {text}
+      {!!icon && <img src={icon} alt="icon" />}
     </button>
   )
 }
