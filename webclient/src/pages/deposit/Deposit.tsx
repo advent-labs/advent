8942346398326fbf14bd248144fed1f9e3bc698b
jmsdevx <jmsdevx@gmail.com>
2022-03-02 19:29:15 -0600

    add reserve selection logic and deposit forms

--- webclient/src/pages/deposit/Deposit.tsx
+++ webclient/src/pages/deposit/Deposit.tsx
@@ -1,187 +1,116 @@
-import { selectAppUIValues } from '../../redux/ui/appui'
-import { useAppSelector } from '../../redux'
+import { selectAppUIValues, actions as appActions } from '../../redux/ui/appui'
+import { useAppDispatch, useAppSelector } from '../../redux'
+import { actions as uiActions } from '../../redux/ui/depositui'
+import { Context } from '../../App'
+import { useContext } from 'react'
 import Container from '../../blocks/Container'
 import RateTabs from '../../common/RateTabs'
-import AssetBar from '../../common/AssetBar'
-import usdcIcon from '../../assets/usdc.svg'
+import Reserve from '../../common/Reserve'
 import Portfolio from '../../common/Portfolio'
+import { Modal } from '../../common/Modal'
+import DepositFixed from './DepositFixed'
+import DepositVar from './DepositVar'
 
 function Deposit() {
-  const { isFixed } = useAppSelector(selectAppUIValues)
+  const { addresses } = useContext(Context)
+  const dispatch = useAppDispatch()
+  const { isFixed, modalOpen } = useAppSelector(selectAppUIValues)
+  const reserves = useAppSelector((s) => s.reserves.state)
+  const form = isFixed ? <DepositFixed /> : <DepositVar />
 
-  const fixedData = [
+  const mockDataFixed = [
     {
-      icon: usdcIcon,
-      uTokenName: 'USDC',
-      data: [
-        {
-          label: 'Fixed lending rate',
-          value: 4.56,
-          currency: '%',
-          loadedOnce: true,
-        },
-        {
-          label: 'Supplied by me',
-          value: 45000.32,
-          currency: 'USDC',
-          loadedOnce: true,
-        },
-        {
-          label: 'My wallet balance',
-          value: 10453000,
-          currency: 'USDC',
-          loadedOnce: true,
-        },
-      ],
+      label: 'Fixed lending rate',
+      value: 4.56,
+      currency: '%',
+      loadedOnce: true,
     },
     {
-      icon: usdcIcon,
-      uTokenName: 'USDC',
-      data: [
-        {
-          label: 'Fixed lending rate',
-          value: 4.56,
-          currency: '%',
-          loadedOnce: true,
-        },
-        {
-          label: 'Supplied by me',
-          value: 45000.32,
-          currency: 'USDC',
-          loadedOnce: true,
-        },
-        {
-          label: 'My wallet balance',
-          value: 10453000,
-          currency: 'USDC',
-          loadedOnce: true,
-        },
-      ],
+      label: 'Supplied by me',
+      value: 45000.32,
+      currency: 'USDC',
+      loadedOnce: true,
     },
     {
-      icon: usdcIcon,
-      uTokenName: 'USDC',
-      data: [
-        {
-          label: 'Fixed lending rate',
-          value: 4.56,
-          currency: '%',
-          loadedOnce: true,
-        },
-        {
-          label: 'Supplied by me',
-          value: 45000.32,
-          currency: 'USDC',
-          loadedOnce: true,
-        },
-        {
-          label: 'My wallet balance',
-          value: 10453000,
-          currency: 'USDC',
-          loadedOnce: true,
-        },
-      ],
+      label: 'My wallet balance',
+      value: 10453000,
+      currency: 'USDC',
+      loadedOnce: true,
     },
   ]
 
-  const variableData = [
+  const mockDataVar = [
     {
-      icon: usdcIcon,
-      uTokenName: 'USDC',
-      data: [
-        {
-          label: 'Variable lending rate',
-          value: 8.36,
-          currency: '%',
-          loadedOnce: true,
-        },
-        {
-          label: 'Supplied by me',
-          value: 0,
-          currency: 'USDC',
-          loadedOnce: true,
-        },
-        {
-          label: 'My wallet balance',
-          value: 10453000,
-          currency: 'USDC',
-          loadedOnce: true,
-        },
-      ],
+      label: 'Variable lending rate',
+      value: 8.36,
+      currency: '%',
+      loadedOnce: true,
     },
     {
-      icon: usdcIcon,
-      uTokenName: 'USDC',
-      data: [
-        {
-          label: 'Variable lending rate',
-          value: 8.36,
-          currency: '%',
-          loadedOnce: true,
-        },
-        {
-          label: 'Supplied by me',
-          value: 0,
-          currency: 'USDC',
-          loadedOnce: true,
-        },
-        {
-          label: 'My wallet balance',
-          value: 10453000,
-          currency: 'USDC',
-          loadedOnce: true,
-        },
-      ],
+      label: 'Supplied by me',
+      value: 0,
+      currency: 'USDC',
+      loadedOnce: true,
     },
     {
-      icon: usdcIcon,
-      uTokenName: 'USDC',
-      data: [
-        {
-          label: 'Variable lending rate',
-          value: 8.36,
-          currency: '%',
-          loadedOnce: true,
-        },
-        {
-          label: 'Supplied by me',
-          value: 0,
-          currency: 'USDC',
-          loadedOnce: true,
-        },
-        {
-          label: 'My wallet balance',
-          value: 10453000,
-          currency: 'USDC',
-          loadedOnce: true,
-        },
-      ],
+      label: 'My wallet balance',
+      value: 10453000,
+      currency: 'USDC',
+      loadedOnce: true,
     },
   ]
 
-  const assetRows = isFixed
+  const fixedData = reserves.map((e, i) => {
+    const mintMeta = addresses?.mintMetaMap[e.token]
+    const { name, icon } = mintMeta
+    return {
+      icon: icon,
+      uTokenName: name,
+      mint: e.token,
+      data: mockDataFixed,
+    }
+  })
+
+  const varData = reserves.map((e, i) => {
+    const mintMeta = addresses?.mintMetaMap[e.token]
+    const { name, icon } = mintMeta
+    return {
+      icon: icon,
+      uTokenName: name,
+      mint: e.token,
+      data: mockDataVar,
+    }
+  })
+
+  const onReserveClick = (v: string) => {
+    dispatch(uiActions.setToken(v))
+    dispatch(appActions.setModalOpen())
+  }
+
+  const reserveRows = isFixed
     ? fixedData.map((e, i) => {
-        return <AssetBar {...e} key={i} />
+        return <Reserve {...e} key={i} action={() => onReserveClick(e.mint)} />
       })
-    : variableData.map((e, i) => {
-        return <AssetBar {...e} key={i} />
+    : varData.map((e, i) => {
+        return <Reserve {...e} key={i} action={() => onReserveClick(e.mint)} />
       })
 
   return (
-    <div className="columns">
-      <div className="column is-9">
-        <Container type="dark" id="deposit">
+    <>
+      <div className="columns">
+        <Container type="dark" id="reserve-list" xtra="column is-9">
           <h1>Lend crypto and get yield</h1>
           <RateTabs />
-          <div className="mt-5 is-full-width">{assetRows}</div>
+          <div className="mt-5 is-full-width">{reserveRows}</div>
         </Container>
-      </div>
-      <div className="column is-3">
-        <Container type="light" id="portfolio">
+        <Container type="light" id="portfolio" xtra="column is-3">
           <Portfolio />
         </Container>
       </div>
-    </div>
+      <Modal open={modalOpen} onClose={() => dispatch(appActions.closeModal())}>
+        {isFixed ? <DepositFixed /> : <DepositVar />}
+      </Modal>
+    </>
   )
 }
 
--- webclient/src/redux/ui/depositui.ts
+++ webclient/src/redux/ui/depositui.ts
@@ -1,6 +1,6 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
-import { RootState } from ".."
-import { actions as reservesActions } from "../reducer/reserves"
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { RootState } from '..'
+import { actions as reservesActions } from '../reducer/reserves'
 export interface DepositRequestedPayload {
   amount: number
   token: string
@@ -12,17 +12,23 @@ export type DepositUI = {
   amount: string
   token: string
   busy: boolean
+  tab: string
+  inputVal: string
+  inputTime: string
 }
 
 const initialState: DepositUI = {
-  duration: "1",
-  amount: "",
-  token: "",
+  duration: '1',
+  amount: '',
+  token: '',
   busy: false,
+  tab: 'Lend',
+  inputVal: '',
+  inputTime: '',
 }
 
 export const depositUI = createSlice({
-  name: "depositui",
+  name: 'depositui',
   initialState,
   reducers: {
     setAmount: (s: DepositUI, action: PayloadAction<string>) => {
@@ -39,8 +45,17 @@ export const depositUI = createSlice({
     },
     depositSucceed: (s) => {
       s.busy = false
-      s.amount = ""
-      s.duration = "1"
+      s.amount = ''
+      s.duration = '1'
+    },
+    setTab: (s: DepositUI, action: PayloadAction<string>) => {
+      s.tab = action.payload
+    },
+    inputHasChanged: (s: DepositUI, action: PayloadAction<string>) => {
+      s.inputVal = action.payload
+    },
+    inputTimeHasChanged: (s: DepositUI, action: PayloadAction<string>) => {
+      s.inputTime = action.payload
     },
   },
   extraReducers: (builder) => {
@@ -56,4 +71,7 @@ export const actions = depositUI.actions
 export const selectDepositUIValues = (s: RootState) => ({
   amount: parseFloat(s.depositui.amount) || 0,
   duration: parseFloat(s.depositui.duration) || 0,
+  tab: s.depositui.tab,
+  inputVal: s.depositui.inputVal,
+  inputTime: s.depositui.inputTime,
 })
