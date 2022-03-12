b930d206b443cbfe4e859c5117685928f35a3613
Thomas Ruble <sketchbang@gmail.com>
2022-03-12 10:25:40 -0600

    add rate

--- webclient/src/pages/dash/UserVariableDeposits.tsx
+++ webclient/src/pages/dash/UserVariableDeposits.tsx
@@ -14,6 +14,7 @@ export function UserVariableDeposits() {
 
       <div>Collateral Percentage: {b.collateralPercentage}</div>
       <div>Total amount deposited: {b.totalDeposited}</div>
+      <div>Rate: {b.rate}</div>
     </div>
   ))
 
--- webclient/src/store/selectors.ts
+++ webclient/src/store/selectors.ts
@@ -9,11 +9,13 @@ export const selectVariableDeposits = (m: MintMetaMap) => (s: RootState) => {
     const balances = deposits[d.token]
     const collateralPercentage = balances.collateral / balances.depositNotes
     const { name, icon } = m[d.token]
-
+    // TODO
+    const rate = 0.073
     return {
       token: d.token,
       name,
       icon,
+      rate,
       collateralPercentage,
       collateralAmount: balances.collateral,
       totalDeposited: balances.depositNotes,
