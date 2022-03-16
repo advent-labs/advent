7970878856a1a39417729c1a6c0861f8291b2c06
Thomas Ruble <sketchbang@gmail.com>
2022-03-16 17:31:30 -0500

    simplify fake data

--- webclient/src/common/Reserve.tsx
+++ webclient/src/common/Reserve.tsx
@@ -1,27 +1,18 @@
-export interface ReserveData {
-  value: number
-  currency: string
-  loadedOnce: boolean
-  icon?: string
-}
-
 export interface ReserveProps {
   icon: string
   uTokenName: string
-  data?: ReserveData[]
+  dataPoints: (string | number)[]
   action: any
 }
 
-function Reserve({ uTokenName, icon, data, action }: ReserveProps) {
-  let dataDisplay = data?.map((e, i) => {
+function Reserve({ uTokenName, icon, dataPoints, action }: ReserveProps) {
+  let dataDisplay = dataPoints?.map((e, i) => {
     return (
       <div
         key={i}
         className="reserve-data is-flex is-align-items-center column is-3"
       >
-        {!!e.icon && <img src={e.icon} alt="rate" />}
-        <p className="text__large-m is-black">{e.value}</p>
-        <p className="text__large-m is-black ml-1">{e.currency}</p>
+        <p className="text__large-m is-black">{e}</p>
       </div>
     )
   })
